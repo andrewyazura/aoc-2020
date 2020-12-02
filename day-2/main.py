333157e418a9f6c695f8af6b1290c7149af855f4
Andrew Yatsura <andrewyazura203@gmail.com>
2020-12-02 22:43:27 +0200

    Day 2 solutions

---
+++ day-2/main.py
@@ -0,0 +1,41 @@
+import re
+
+puzzle_input = []
+
+with open('day-2/input.txt', 'r') as file:
+    puzzle_input = [i.strip() for i in file.readlines()]
+
+puzzle_template = '(\d+)-(\d+) (\w): (\w+)'
+puzzle_regex = re.compile(puzzle_template)
+
+
+def part_1(puzzle_input):
+    valid = 0
+
+    for entry in puzzle_input:
+        a, b, letter, password = puzzle_regex.match(entry).groups()
+        amount = password.count(letter)
+
+        if int(a) <= amount <= int(b):
+            valid += 1
+
+    return valid
+
+
+def part_2(puzzle_input):
+    valid = 0
+
+    for entry in puzzle_input:
+        a, b, letter, password = puzzle_regex.match(entry).groups()
+        a = int(a) - 1
+        b = int(b) - 1
+
+        if (password[a] == letter or password[b] == letter) \
+                and not (password[a] == letter and password[b] == letter):
+            valid += 1
+
+    return valid
+
+
+print(part_1(puzzle_input))
+print(part_2(puzzle_input))
