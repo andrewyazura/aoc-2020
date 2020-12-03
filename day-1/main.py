3a691da0c492b89daed6867ae523300269055702
Andrew Yatsura <andrewyazura203@gmail.com>
2020-12-03 09:41:59 +0200

    Day 3 solutions

--- day-1/main.py
+++ day-1/main.py
@@ -1,23 +1,18 @@
 import itertools
 
+import numpy
+
 puzzle_input = []
 
 with open('day-1/input.txt', 'r') as file:
     puzzle_input = [int(i.strip()) for i in file.readlines()]
 
 
-def mul(*args):
-    r = 1
-    for i in args:
-        r *= i
-    return r
-
-
 def solution(numbers, n, result):
     a = [numbers] * n
     for i in itertools.product(*a):
         if sum(i) == result:
-            return mul(*i)
+            return numpy.prod(i)
 
 
 def part_1(puzzle_input):
---
+++ day-3/main.py
@@ -0,0 +1,34 @@
+import numpy
+
+puzzle_input = []
+
+with open('day-3/input.txt', 'r') as file:
+    puzzle_input = [i.strip() for i in file.readlines()]
+
+
+def check_slope(grid, row_shift, column_shift=1):
+    trees = 0
+    shift = row_shift
+
+    for row in grid[column_shift::column_shift]:
+        if row[shift] == '#':
+            trees += 1
+
+        shift = (shift + row_shift) % len(row)
+
+    return trees
+
+
+def part_1(puzzle_input):
+    return check_slope(puzzle_input, 3)
+
+
+def part_2(puzzle_input):
+    trees = [check_slope(puzzle_input, shift, 1) for shift in range(1, 8, 2)]
+    trees.append(check_slope(puzzle_input, 1, 2))
+
+    return numpy.prod(trees)
+
+
+print(part_1(puzzle_input))
+print(part_2(puzzle_input))
